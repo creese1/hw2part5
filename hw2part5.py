fd5d64fe8a9dd94cc821a01bf5a74e4661282bd0
Chuck Reese <creese1@student.waubonsee.edu>
2020-03-07 16:55:53 -0600

    Initial version of HW1 Part 5

---
+++ hw2part5.py
@@ -0,0 +1,14 @@
+def getNum():
+    needNum = True
+    while needNum:
+        value = int(input("Enter a number: "))
+        if (value > 0):
+            needNum = False
+    return value
+
+
+num1 = getNum()
+num2 = getNum()
+
+if(num1 > num2):
+    print("num 1 is bigger than num 2")
